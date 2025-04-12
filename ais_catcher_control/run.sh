b2c30901e067649139f5e76c8ec629abe90dbb3e
CzarofAK <64549444+CzarofAK@users.noreply.github.com>
2025-04-12 21:23:29 +0200

    Update run.sh

--- ais_catcher_control/run.sh
+++ ais_catcher_control/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# Start AIS-catcher-control
+# Enable verbose logging
+set -e
+
 echo "Starting AIS-catcher-control..."
-/usr/bin/AIS-catcher-control
+
+# Start AIS-catcher-control and log output
+/usr/bin/AIS-catcher-control 2>&1 | tee /dev/stdout
