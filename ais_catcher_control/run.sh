2a93ed287c0a51ec5cbdbbf58b378d4e24106e41
CzarofAK <64549444+CzarofAK@users.noreply.github.com>
2025-04-12 22:29:39 +0200

    Update run.sh

--- ais_catcher_control/run.sh
+++ ais_catcher_control/run.sh
@@ -4,19 +4,8 @@ set -e
 # Import Bashio
 . /usr/lib/bashio/bashio.sh
 
-# Read configuration options
-GPSD_IP=$(bashio::config 'gpsd_ip')
-GPSD_PORT=$(bashio::config 'gpsd_port')
-SHARE_LOC=$(bashio::config 'share_loc')
+# Log the start of the service
+bashio::log.info "Starting AIS-catcher-control..."
 
-# Build the AIS-catcher-control command
-CMD="/usr/bin/AIS-catcher-control -t gpsd ${GPSD_IP} ${GPSD_PORT} -N 8110"
-
-# Add SHARE_LOC if enabled
-if [ "$SHARE_LOC" = true ]; then
-  CMD="$CMD SHARE_LOC"
-fi
-
-# Log the command and start the service
-bashio::log.info "Starting AIS-catcher-control with command: $CMD"
-exec $CMD
+# Start AIS-catcher-control
+exec /usr/bin/AIS-catcher-control
