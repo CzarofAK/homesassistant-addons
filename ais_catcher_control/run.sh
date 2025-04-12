8166f61289829dd60c9f34b99fa1479158eba6d1
CzarofAK <64549444+CzarofAK@users.noreply.github.com>
2025-04-12 21:04:20 +0200

    Update run.sh

--- ais_catcher_control/run.sh
+++ ais_catcher_control/run.sh
@@ -1,3 +1,19 @@
-#!/bin/bash
-# Start AIS-catcher-control
-/usr/bin/AIS-catcher-control
+#!/usr/bin/env bash
+CONFIG_PATH=/data/options.json
+
+# Read configuration options
+GPSD_IP=$(bashio::config 'gpsd_ip')
+GPSD_PORT=$(bashio::config 'gpsd_port')
+SHARE_LOC=$(bashio::config 'share_loc')
+
+# Build the AIS-catcher-control command
+CMD="/usr/bin/AIS-catcher-control -t gpsd ${GPSD_IP} ${GPSD_PORT} -N 8110"
+
+# Add SHARE_LOC if enabled
+if [ "$SHARE_LOC" = true ]; then
+  CMD="$CMD SHARE_LOC"
+fi
+
+# Run the command
+bashio::log.info "Starting AIS-catcher-control with command: $CMD"
+$CMD
